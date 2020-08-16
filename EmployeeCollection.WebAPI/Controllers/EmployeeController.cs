5bb9d829fae018bea1a317773a562ea17a71efc5
Git Server Admin <admin@git-server>
2020-08-16 15:39:42 +0000

    Add initial repository

---
+++ EmployeeCollection.WebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using EmployeeCollection.WebAPI.Data;
+using EmployeeCollection.WebAPI.Services;
+
+namespace EmployeeCollection.WebAPI.Controllers
+{
+}
